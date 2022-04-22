f7c800ced355f25d3951baba32d3a2154af8a5fa
Ben Life <77246839+benlife5@users.noreply.github.com>
2022-04-22 14:35:52 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import Question from "./Question.js"
+import {useEffect, useState} from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [triviaData, setTriviaData] = useState();
+  const [totalAnswered, setTotalAnswered] = useState(0);
+  const [totalCorrect, setTotalCorrect] = useState(0);
+
+  const percentage = totalAnswered !== 0? Math.round(totalCorrect/totalAnswered * 100) : 0;
+
+  const incrementCorrect = () => {
+    setTotalCorrect(totalCorrect + 1)
+    setTotalAnswered(totalAnswered + 1)
+  }
+  const incrementIncorrect = () => {
+    setTotalAnswered(totalAnswered + 1)
+  }
+
+  const generateQuestions = () => {
+    fetch("https://opentdb.com/api.php?amount=10")
+    .then((res) => res.json())
+    .then((data) => setTriviaData(data.results))
+  }
+
+  useEffect(() => {
+    generateQuestions()
+  }, [])
+
+  if (triviaData) {
+    return (
+      <div className="App">
+        <h1>Trivia</h1>
+        <p>Your score: {totalCorrect} / {totalAnswered} ({percentage}%)</p>
+        <button onClick={generateQuestions}>New Questions</button>
+        {triviaData.map((question) => <Question question_data={question} key={question.question} correct={incrementCorrect} incorrect={incrementIncorrect} />)}   
+      </div>
+    );
+  } else {
+    return(<div className="App"><h1>Trivia</h1><h2>Loading...</h2></div>)
+  }
+
 }
 
 export default App;
---
+++ src/Question.js
@@ -0,0 +1,51 @@
+import {useState} from "react"
+import Button from '@mui/material/Button';
+const HTMLDecoderEncoder = require("html-encoder-decoder");
+
+
+const Question = (props) => {
+  // Create state variables for each question
+  const [isAnswered, setIsAnswered] = useState(false);
+  const [isCorrect, setIsCorrect] = useState()
+  const [allAnswers, setAllAnswers] = useState([]); 
+
+  // optional, just makes syntax shorter
+  const data = props.question_data;
+
+  // insert the correct answer into the incorrect answers
+  if (allAnswers.length === 0) { // make sure this only happens once
+    const answers = [...data.incorrect_answers]  // make a copy of the incorrect answers (best to not modify props)
+    answers.splice(Math.floor(Math.random() * (allAnswers.length + 1)), 0, data.correct_answer) // randomize position
+    setAllAnswers(answers) 
+  }
+
+  // When an answer is click, updated state
+  const handleClick = (answer) => {
+    setIsAnswered(true)
+    if (answer === data.correct_answer) {
+      setIsCorrect(true)
+      props.correct()  // update parent state
+    } else {
+      setIsCorrect(false)
+      props.incorrect()  // update parent state
+    }
+  }
+
+  // determine question color based on state
+  let questionColor = "black";
+  if (isAnswered) {
+    questionColor = isCorrect? "green" : "red"
+  }
+
+  return(
+    <div>
+      <h2 style={{color: questionColor}}>{HTMLDecoderEncoder.decode(data.question)}</h2>
+      {/* {JSON.stringify(data)} */}
+      {allAnswers.map((answer)=> <Button variant="outlined" style={{marginLeft: "1em"}} disabled={isAnswered} key={answer} onClick={() => handleClick(answer)}>{HTMLDecoderEncoder.decode(answer)}</Button>)}
+      {isAnswered && <p>The correct answer was {HTMLDecoderEncoder.decode(data.correct_answer)}.</p>}
+      <br />
+    </div>
+  )
+}
+
+export default Question
